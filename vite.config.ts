a73d87daa42ff63f10399847cd2e3b771a2ef721
fmrtWangk <49018937+fmrtWangk@users.noreply.github.com>
2026-06-16 09:07:38 +0000

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jubilant-guacamole/', // Replace with your actual repo name
   plugins: [
     vue(),
     vueDevTools(),
